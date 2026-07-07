133d534817ccb0e463c3256aa7e744ea41c95867
nixifyxyz <jamaliudinsa5@gmail.com>
2026-07-07 19:58:48 +0700

    Create page.tsx

---
+++ dashboard/page.tsx
@@ -0,0 +1,20 @@
+import TokenCard from "@/components/TokenCard";
+import WalletCard from "@/components/WalletCard";
+
+export default function Dashboard() {
+
+  return (
+
+    <main>
+
+      <h1>Nixify Intelligence Dashboard</h1>
+
+      <TokenCard />
+
+      <WalletCard />
+
+    </main>
+
+  );
+
+}
